Fix: Resolve RSCV constraint bug, correct variance margins for relaxation, and fix CI Coveralls (#21)

src/smt_optim/acquisition_strategies/mfsego.py
@@ -100,17 +100,17 @@ class MFSEGO(AcquisitionStrategy):
         self.acq_func = kwargs.pop("acq_func", log_ei)                          # expected_improvement, log_ei
         self.fmin_crit = kwargs.pop("fmin_crit", "min_rscv")                    # broken -> to be removed (min_rscv, fmin, mean_rscv)
         # self.sub_optimizer = kwargs.pop("sub_optimizer", "COBYLA")
-        self.n_start = kwargs.pop("n_start", None)                              # optimizer multistart
-        self.fidelity_crit = kwargs.pop("fidelity_crit", "obj-only")            # obj-only, average, optimistic, pessimistic
-        self.select_fidelity = kwargs.pop("select_fidelity", True)              # if set to False, will always sample (LF+HF)
-        self.min_rscv_first = kwargs.pop("min_rscv_first", False)               # broken -> to be fixed!
-        self.filter_rscv = kwargs.pop("filter_rscv", False)                     # broken -> to be fixed!
-        self.optimize_best = kwargs.pop("optimize_best", False)                 # broken -> to be fixed!
-        self.relax_constraints = kwargs.pop("relax_constraints", False)         # broken -> to be fixed!
-        self.cr_override = kwargs.pop("cr_override", None)                      # override optimizer Cost Ratio
-        self.sp_method = kwargs.pop("sp_method", "SLSQP")                       # SciPy optimizer method
-        self.sp_tol = kwargs.pop("sp_tol", np.sqrt(np.finfo(float).eps))        # SciPy optimizer tolerance
-        self.var_red_corr = kwargs.pop("var_red_corr", None)                    # Variance reduction correction scheme
+        self.n_start = kwargs.pop("n_start", None)  # optimizer multistart
+        self.fidelity_crit = kwargs.pop("fidelity_crit", "obj-only")  # obj-only, average, optimistic, pessimistic
+        self.select_fidelity = kwargs.pop("select_fidelity", True)  # if set to False, will always sample (LF+HF)
+        self.min_rscv_first = kwargs.pop("min_rscv_first", False)
+        self.filter_rscv = kwargs.pop("filter_rscv", False)
+        self.optimize_best = kwargs.pop("optimize_best", False)
+        self.relax_constraints = kwargs.pop("relax_constraints", False)
+        self.cr_override = kwargs.pop("cr_override", None)  # override optimizer Cost Ratio
+        self.sp_method = kwargs.pop("sp_method", "SLSQP")  # SciPy optimizer method
+        self.sp_tol = kwargs.pop("sp_tol", np.sqrt(np.finfo(float).eps))  # SciPy optimizer tolerance
+        self.var_red_corr = kwargs.pop("var_red_corr", None)  # Variance reduction correction scheme
 
         self.seed = kwargs.pop("seed", None)
 
@@ -258,53 +258,7 @@ class MFSEGO(AcquisitionStrategy):
         return scipy_acq_func
 
     def build_scipy_constraints(self, state: State) -> list[dict]:
-
-        scipy_cstr = []
-
-        def append_sp_cstr(func: Callable, type: str) -> None:
-            scipy_cstr.append({
-                "fun": func,
-                "type": type,
-            })
-
-        # TODO: re-implement constraint relaxation
-        # def sp_constraint(x):
-        #     x = x.reshape(1, -1)
-        #     mu = mu_func(x).item()
-        #
-        #     if relax:
-        #         s = np.sqrt(s2_func(x).item())
-        #         if type == "ineq":
-        #             return -(mu - 3 * s)
-        #         elif type == "eq":
-        #             return -(np.abs(mu) - 3 * s)
-        #
-        #     return -mu
-        #
-        # return sp_constraint
-
-        def sp_constraint(x, model):
-            x = x.reshape(1, -1)
-            mu = model.predict_values(x)
-            return mu.item()
-
-
-        for c_id, c_config in enumerate(state.problem.cstr_configs):
-
-            if c_config.equal is not None:
-                func = lambda x, f=sp_constraint, value=state.cstr_equal[c_id], m=state.cstr_models[c_id]: f(x, m) - value
-                append_sp_cstr(func, "eq")
-
-            else:
-                if c_config.lower is not None:
-                    func = lambda x, f=sp_constraint, value=state.cstr_lower[c_id], m=state.cstr_models[c_id]: - value + f(x, m)
-                    append_sp_cstr(func, "ineq")
-
-                if c_config.upper is not None:
-                    func = lambda x, f=sp_constraint, value=state.cstr_upper[c_id], m=state.cstr_models[c_id]: - f(x, m) + value
-                    append_sp_cstr(func, "ineq")
-
-        return scipy_cstr
+        return build_scipy_constraints(state, self.relax_constraints)
 
 
     def get_fidelity(self, next_x: np.ndarray, state: State) -> list[int]:
@@ -558,8 +512,81 @@ def compute_all_s2_red_norm(x_pred: np.ndarray, costs: list[float], surrogates: 
     return s2_red_norm
 
 
-def select_fidelity_level(x_pred: np.ndarray, costs: list[float], all_surrogates: list[Surrogate],
-                          criterion: str = "pessimistic", corr_method=None) -> tuple[np.ndarray, np.ndarray]:
+def build_scipy_constraints(state: State, relax: bool = False) -> list[dict]:
+
+    scipy_cstr = []
+
+    def append_sp_cstr(func: Callable, type: str) -> None:
+        scipy_cstr.append(
+            {
+                "fun": func,
+                "type": type,
+            }
+        )
+
+    for c_id, c_config in enumerate(state.problem.cstr_configs):
+        if c_config.equal is not None:
+
+            def func(
+                x,
+                value=state.cstr_equal[c_id],
+                m=state.cstr_models[c_id],
+                r=relax,
+            ):
+                x = x.reshape(1, -1)
+                mu = m.predict_values(x).item()
+                if r:
+                    s = np.sqrt(max(0.0, m.predict_variances(x).item()))
+                    return -np.abs(mu - value) + 3 * s
+                return mu - value
+
+            append_sp_cstr(func, "ineq" if relax else "eq")
+
+        else:
+            if c_config.lower is not None:
+
+                def func(
+                    x,
+                    value=state.cstr_lower[c_id],
+                    m=state.cstr_models[c_id],
+                    r=relax,
+                ):
+                    x = x.reshape(1, -1)
+                    mu = m.predict_values(x).item()
+                    if r:
+                        s = np.sqrt(max(0.0, m.predict_variances(x).item()))
+                        return (mu + 3 * s) - value
+                    return mu - value
+
+                append_sp_cstr(func, "ineq")
+
+            if c_config.upper is not None:
+
+                def func(
+                    x,
+                    value=state.cstr_upper[c_id],
+                    m=state.cstr_models[c_id],
+                    r=relax,
+                ):
+                    x = x.reshape(1, -1)
+                    mu = m.predict_values(x).item()
+                    if r:
+                        s = np.sqrt(max(0.0, m.predict_variances(x).item()))
+                        return value - (mu - 3 * s)
+                    return value - mu
+
+                append_sp_cstr(func, "ineq")
+
+    return scipy_cstr
+
+
+def select_fidelity_level(
+    x_pred: np.ndarray,
+    costs: list[float],
+    all_surrogates: list[Surrogate],
+    criterion: str = "pessimistic",
+    corr_method=None,
+) -> tuple[np.ndarray, np.ndarray]:
     """
     Select the highest fidelity level to sample based on the `criterion`.
 

src/smt_optim/utils/constraints.py
@@ -91,10 +91,11 @@ def compute_rscv_sp(x: np.ndarray, cstr_list: list[dict]) -> float:
     for c_id, c_dict in enumerate(cstr_list):
 
         c_type = c_dict["type"]
+        val = float(c_dict["fun"](x))
         if c_type == "ineq":
-            scv[c_id] = min(0, c_dict["fun"](x))**2
+            scv[c_id] = min(0.0, val) ** 2
         elif c_type == "eq":
-            scv[c_id] = c_dict["fun"](x)**2
+            scv[c_id] = val ** 2
 
     return np.sqrt(np.sum(scv))
 